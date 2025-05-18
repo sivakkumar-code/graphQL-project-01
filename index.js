9564a5e03a1625cb26810ed5d45450d95319d6da
sivakkumar <sivakkumarinfo@gmail.com>
2025-05-18 23:06:28 +0530

    created the resolver for games and also mentioned the query that we will be doing in the frontend

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+
+// db
+import db from "./_db";
+
+// types
 import { typeDefs } from "./schema";
 
 // constants
 const port = 4000;
+
+// resolver
+const resolvers = {
+    Query: {
+        games(){
+            return db.games;
+        }
+    }
+}
+
+/*
+ query for games will look something like this
+ games {
+    title
+ }
+
+ in the resolver we are returning the whole games array.
+ but graphQL is doing it's magic, by sending array of items with only the requested items from the frontend
+ */
 
 // server setup
 const server = new ApolloServer({
@@ -12,8 +36,8 @@ const server = new ApolloServer({
     // resolvers
 });
 
-const {url} = await startStandaloneServer(server, {
-    listen: {port}
+const { url } = await startStandaloneServer(server, {
+    listen: { port }
 })
 
 console.log("Server ready at port:", port)

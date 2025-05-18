2ffd3edc402a783d8abdc0cf6aaf273e6281944c
sivakkumar <sivakkumarinfo@gmail.com>
2025-05-18 23:16:42 +0530

    added the resolver to ApolloServer

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
 // db
-import db from "./_db";
+import db from "./_db.js";
 
 // types
-import { typeDefs } from "./schema";
+import { typeDefs } from "./schema.js";
 
 // constants
 const port = 4000;
@@ -38,8 +38,9 @@ const resolvers = {
 // server setup
 const server = new ApolloServer({
     // typeDefs - definitions of types of data
-    typeDefs
+    typeDefs,
     // resolvers
+    resolvers
 });
 
 const { url } = await startStandaloneServer(server, {
